e4b72f0bf8efcc2bfab9e7e69ed61e606068c5b1
sarautaher <tahersara591@gmail.com>
2024-01-23 03:03:55 +0200

    sec commit

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-let list_Home=$(".list_Home").outerWidth();
+let list_Home=$(".optionBox ").outerWidth();
 $(document).ready(function(){
     $('.optionBox').animate({left:`-${list_Home}`},10,function(){
     $('#loading').fadeOut(1000 ,()=>{
@@ -12,19 +12,19 @@ $(document).ready(function(){
 let about=$('#about').offset().top;
 $('#ToggleBtn').click(function(){
      $('.optionBox ').animate({left:`0px`},1000); 
-     $('#ToggleBtn   ').animate({marginLeft:`${list_Home}`},1000)
+     $('#ToggleBtn,.home-info   ').animate({marginLeft:`${list_Home}`},1000)
 })
 $('#closebtn').click(function(){
     $('.optionBox').animate({left:`-${list_Home}`},1000);
-    $('#ToggleBtn ').animate({marginLeft:`0px`},1000)
+    $('#ToggleBtn ,.home-info').animate({marginLeft:`0px`},1000)
 })
 $(window).scroll( function(){
     let Wscroll=$(window).scrollTop();
     if(Wscroll>about+10){
-    $(".list_Home").css("backgroundColor","rgba(0, 0, 0, 0.3)");
+    $(".optionBox ").css("backgroundColor","rgba(0, 0, 0, 0.3)");
  }
  else{
-    $(".list_Home").css("backgroundColor","black");
+    $(".optionBox ").css("backgroundColor","black");
  }
     
 })
@@ -38,9 +38,7 @@ let counter2=setInterval( function counterdown( ){
 let countDate=new Date(counter);
 countDate=(countDate.getTime());
 let countHere=new Date().getTime();
-
 countchages=(countDate-countHere);
-
 let day=Math.floor(countchages/(1000*24*60*60));
 let hour=Math.floor((countchages%(1000*(24*60*60)))/(1000*3600));
 let min=Math.floor((countchages %(1000*3600))/(1000*60));
